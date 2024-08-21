ddc6e527b0ea395e5e3bb7ea20e8014b5bdbcae3
dungvt05hc <vothaidung9611@gmail.com>
2024-08-21 23:01:14 +0700

    up

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -16,7 +16,6 @@ from opencensus.trace.propagation.trace_context_http_header_format import TraceC
 from opencensus.ext.flask.flask_middleware import FlaskMiddleware
 
 # App Insights
-# TODO: Import required libraries for App Insights
 INSTRUMENTATION_KEY = '7849fd59-90e3-48aa-9ea9-ebd86837947e'
 
 # Logging
@@ -28,7 +27,7 @@ logger.addHandler(AzureLogHandler(connection_string=f'InstrumentationKey={INSTRU
 exporter = MetricsExporter(connection_string=f'InstrumentationKey={INSTRUMENTATION_KEY}')
 
 # Tracing
-config_integration.trace_integrations(['requests'])
+config_integration.trace_integrations(['logging', 'requests'])
 tracer = Tracer(exporter=AzureExporter(connection_string=f'InstrumentationKey={INSTRUMENTATION_KEY}'), 
                 sampler=ProbabilitySampler(1.0))
 
@@ -45,17 +44,17 @@ middleware = FlaskMiddleware(
 # Load configurations from environment or config file
 app.config.from_pyfile('config_file.cfg')
 
-if ("VOTE1VALUE" in os.environ and os.environ['VOTE1VALUE']):
+if "VOTE1VALUE" in os.environ and os.environ['VOTE1VALUE']:
     button1 = os.environ['VOTE1VALUE']
 else:
     button1 = app.config['VOTE1VALUE']
 
-if ("VOTE2VALUE" in os.environ and os.environ['VOTE2VALUE']):
+if "VOTE2VALUE" in os.environ and os.environ['VOTE2VALUE']:
     button2 = os.environ['VOTE2VALUE']
 else:
     button2 = app.config['VOTE2VALUE']
 
-if ("TITLE" in os.environ and os.environ['TITLE']):
+if "TITLE" in os.environ and os.environ['TITLE']:
     title = os.environ['TITLE']
 else:
     title = app.config['TITLE']
@@ -68,21 +67,23 @@ if app.config['SHOWHOST'] == "true":
     title = socket.gethostname()
 
 # Init Redis
-if not r.get(button1): r.set(button1,0)
-if not r.get(button2): r.set(button2,0)
+if not r.get(button1): r.set(button1, 0)
+if not r.get(button2): r.set(button2, 0)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
 
     if request.method == 'GET':
-
         # Get current values
         vote1 = r.get(button1).decode('utf-8')
-        # TODO: use tracer object to trace cat vote
-        tracer.span(name="Get Cat Vote")  # Trace the cat vote
         vote2 = r.get(button2).decode('utf-8')
-        # TODO: use tracer object to trace dog vote
-        tracer.span(name="Get Dog Vote")  # Trace the dog vote
+
+        # Trace votes
+        with tracer.span(name="Cats Vote"):
+            pass  # Just creating the span context
+
+        with tracer.span(name="Dogs Vote"):
+            pass  # Just creating the span context
 
         # Return index with values
         return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
@@ -90,27 +91,23 @@ def index():
     elif request.method == 'POST':
 
         if request.form['vote'] == 'reset':
-
             # Empty table and return results
-            r.set(button1,0)
-            r.set(button2,0)
+            r.set(button1, 0)
+            r.set(button2, 0)
             vote1 = r.get(button1).decode('utf-8')
             properties = {'custom_dimensions': {'Cats Vote': vote1}}
-            # TODO: use logger object to log cat vote
             logger.info('Cat vote reset', extra=properties)  # Log cat vote
 
             vote2 = r.get(button2).decode('utf-8')
             properties = {'custom_dimensions': {'Dogs Vote': vote2}}
-            # TODO: use logger object to log dog vote
             logger.info('Dog vote reset', extra=properties)  # Log dog vote
 
             return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
 
         else:
-
             # Insert vote result into DB
             vote = request.form['vote']
-            r.incr(vote,1)
+            r.incr(vote, 1)
 
             # Get current values
             vote1 = r.get(button1).decode('utf-8')
@@ -126,13 +123,14 @@ def index():
                 logger.info("Dog vote cast", extra={'custom_dimensions': {'vote': 'dogs'}})
 
             # Trace the vote
-            tracer.span(name=f"Vote {vote}")
+            with tracer.span(name=f"Vote {vote}"):
+                pass  # Just creating the span context
 
             # Return results
             return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
 
 if __name__ == "__main__":
-    # TODO: Use the statement below when running locally
+    # Use the statement below when running locally
     app.run() 
-    # TODO: Use the statement below before deployment to VMSS
+    # Use the statement below before deployment to VMSS
     # app.run(host='0.0.0.0', threaded=True, debug=True) # remote
