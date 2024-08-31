4a154f87ee56749a5d50d182c7123a8876046294
dungvt05hc <vothaidung9611@gmail.com>
2024-08-31 13:34:11 +0700

    up

--- create-cluster.sh
+++ create-cluster.sh
@@ -16,27 +16,27 @@ echo "Step 1 - Creating AKS cluster $clusterName"
 # Use either one of the "az aks create" commands below
 # For users working in their personal Azure account
 # This commmand will not work for the Cloud Lab users, because you are not allowed to create Log Analytics workspace for monitoring
-az aks create \
---resource-group $resourceGroup \
---name $clusterName \
---node-count 1 \
---location southeastasia \
---node-vm-size Standard_DS2_v2 \
---enable-addons monitoring \
---generate-ssh-keys
-
-# For Cloud Lab users
 # az aks create \
 # --resource-group $resourceGroup \
 # --name $clusterName \
 # --node-count 1 \
+# --location southeastasia \
+# --node-vm-size Standard_DS2_v2 \
+# --enable-addons monitoring \
 # --generate-ssh-keys
+
+# For Cloud Lab users
+az aks create \
+--resource-group $resourceGroup \
+--name $clusterName \
+--node-count 1 \
+--generate-ssh-keys
 
 # For Cloud Lab users
 # This command will is a substitute for "--enable-addons monitoring" option in the "az aks create"
 # Use the log analytics workspace - Resource ID
 # For Cloud Lab users, go to the existing Log Analytics workspace --> Properties --> Resource ID. Copy it and use in the command below.
-# az aks enable-addons -a monitoring -n $clusterName -g $resourceGroup --workspace-resource-id "/subscriptions/6c39f60b-2bb1-4e37-ad64-faaf30beaca4/resourcegroups/cloud-demo-153430/providers/microsoft.operationalinsights/workspaces/loganalytics-153430"
+az aks enable-addons -a monitoring -n $clusterName -g $resourceGroup --workspace-resource-id "/subscriptions/62d70a26-cec0-4efb-878f-bb4ba7135ee6/resourceGroups/cloud-demo/providers/Microsoft.OperationalInsights/workspaces/loganalytics-267168"
 
 echo "AKS cluster created: $clusterName"
 
@@ -45,8 +45,8 @@ echo "AKS cluster created: $clusterName"
 echo "Step 2 - Getting AKS credentials"
 
 az aks get-credentials \
---resource-group rg-project4 \
---name udacity-cluster \
+--resource-group $resourceGroup \
+--name $clusterName \
 --verbose
 
 echo "Verifying connection to $clusterName"
